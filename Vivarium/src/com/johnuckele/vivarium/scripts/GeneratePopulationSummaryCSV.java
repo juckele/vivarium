a64d69530ab0473be06daa2d739b770cbe31a6c4
John H. Uckele <j@vivarium.io>
2015-05-19 10:03:39 -0400

    Added population standard deviation to the population summary export. Added basic test cases for the population summary script.

--- Vivarium/src/com/johnuckele/vivarium/scripts/GeneratePopulationSummaryCSV.java
+++ Vivarium/src/com/johnuckele/vivarium/scripts/GeneratePopulationSummaryCSV.java
@@ -33,21 +33,27 @@ public class GeneratePopulationSummaryCSV extends Script
 		World w = loadWorld(args[0], Format.JAVA_SERIALIZABLE);
 		CensusRecord census = w.getCensus();
 
-		// Build the CSV data
-		StringBuilder csvStringBuilder = new StringBuilder("unique_population,min_population,max_population,mean_population\n");
+		// Header, this CSV is always a single row so we build the header with the single row
+		StringBuilder csvStringBuilder = new StringBuilder("unique_population,min_population,max_population,mean_population,population_stddev\n");
+
+		// Variable declaration
 		int minPopulation = 0;
 		int maxPopulation = 0;
 		double meanPopulation = 0;
+		double sumOfSquaredErrors = 0;
+		double standardDeviation = 0;
 		long totalPopulationTicks = 0;
 		int lastPopulationRecord = 0;
 		int lastTickRecord = 0;
+
+		// Iterate through and count total population ticks and look for min/max values
 		Iterator<IntTuple> it = census.getRecords().iterator();
 		if(it.hasNext())
 		{
+			// This is the initial population and always has time zero
 			IntTuple record = it.next();
 			minPopulation = record.b;
 			maxPopulation = record.b;
-			totalPopulationTicks += (record.a - lastTickRecord) * lastPopulationRecord;
 			lastPopulationRecord = record.b;
 			lastTickRecord = record.a;
 		}
@@ -66,6 +72,9 @@ public class GeneratePopulationSummaryCSV extends Script
 			lastPopulationRecord = record.b;
 			lastTickRecord = record.a;
 		}
+
+		// Start building data row
+
 		// Unique population
 		csvStringBuilder.append(w.getMaximumUckeleoidID());
 		csvStringBuilder.append(',');
@@ -79,6 +88,27 @@ public class GeneratePopulationSummaryCSV extends Script
 		totalPopulationTicks += (w.getTickCounter() - lastTickRecord) * lastPopulationRecord;
 		meanPopulation = totalPopulationTicks / (double)w.getTickCounter();
 		csvStringBuilder.append(meanPopulation);
+		csvStringBuilder.append(',');
+		// calculate population standard deviation
+		it = census.getRecords().iterator();
+		if(it.hasNext())
+		{
+			// This is the initial population and always has time zero
+			IntTuple record = it.next();
+			lastPopulationRecord = record.b;
+			lastTickRecord = record.a;
+		}
+		while(it.hasNext())
+		{
+			IntTuple record = it.next();
+			sumOfSquaredErrors += (record.a - lastTickRecord) * Math.pow(lastPopulationRecord - meanPopulation, 2);
+			lastPopulationRecord = record.b;
+			lastTickRecord = record.a;
+		}
+		sumOfSquaredErrors += (w.getTickCounter() - lastTickRecord) * Math.pow(lastPopulationRecord - meanPopulation, 2);
+		standardDeviation = Math.sqrt(sumOfSquaredErrors / (double)w.getTickCounter()); 
+		csvStringBuilder.append(standardDeviation);		
+		// end the line
 		csvStringBuilder.append('\n');
 
 		// Save
