0cb84358323b3aafa0346d2344fd5ea38ff1d236
John H. Uckele <j@vivarium.io>
2015-08-11 21:19:01 -0400

    Remove of StringTokenizer for GWT support

--- vivarium-core/src/main/java/com/johnuckele/vivarium/util/HierarchicalListParser.java
+++ vivarium-core/src/main/java/com/johnuckele/vivarium/util/HierarchicalListParser.java
@@ -1,9 +1,10 @@
 package com.johnuckele.vivarium.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class HierarchicalListParser
 {
@@ -59,11 +60,22 @@ public class HierarchicalListParser
         }
         else if (listString.charAt(0) == '[' && listString.charAt(listString.length() - 1) == ']')
         {
-            StringTokenizer tokenizer = new StringTokenizer(listString, "[,]", true);
-            tokenizer.nextToken(); // Pops off the first [ so that the innerParse returns when it hits the last ]
-            ArrayList<Object> list = new ArrayList<Object>();
-            innerParse(list, tokenizer);
-            return list;
+            // Replace 'delimiter' tokens with extra characters to use as actual delimiters
+            listString = listString.replace("[", "[~");
+            listString = listString.replace(",", "~,~");
+            listString = listString.replace("]", "~]");
+            listString = listString.replace("[~~]", "[~]"); // Remove this particular double delimiter
+            // Remove the first and last trash delimiters to avoid getting empty strings at the start and end
+            // listString = listString.substring(1, listString.length() - 1);
+            // Split the strings into tokens, including entries for the token delimiters above
+            List<String> tokenList = new LinkedList<String>(Arrays.asList(listString.split("~")));
+            // Discard the first token, which is always a '[' so that when the recursive function sees the last ']', it
+            // can return correctly
+            tokenList.remove(0);
+            // Parse the token list into the object list
+            ArrayList<Object> objectList = new ArrayList<Object>();
+            innerParse(objectList, tokenList);
+            return objectList;
         }
         else
         {
@@ -72,17 +84,17 @@ public class HierarchicalListParser
         }
     }
 
-    private static void innerParse(List<Object> list, StringTokenizer tokenizer)
+    private static void innerParse(List<Object> list, List<String> tokenList)
     {
         boolean emptyString = true;
-        while (tokenizer.hasMoreTokens())
+        while (tokenList.size() > 0)
         {
-            String token = tokenizer.nextToken();
+            String token = tokenList.remove(0);
             if (token.equals("["))
             {
                 ArrayList<Object> innerList = new ArrayList<Object>();
                 list.add(innerList);
-                innerParse(innerList, tokenizer);
+                innerParse(innerList, tokenList);
                 emptyString = false;
             }
             else if (token.equals(","))
