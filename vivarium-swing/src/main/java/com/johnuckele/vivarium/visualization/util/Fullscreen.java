088a7b46ffcda7bd84da8ee03dcc5d6e2277c2dd
John H. Uckele <j@vivarium.io>
2015-09-17 07:29:19 -0400

    Working full screen (linux only?)

---
+++ vivarium-swing/src/main/java/com/johnuckele/vivarium/visualization/util/Fullscreen.java
@@ -0,0 +1,76 @@
+package com.johnuckele.vivarium.visualization.util;
+
+import java.awt.Window;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLong;
+import com.sun.jna.platform.unix.X11;
+import com.sun.jna.platform.unix.X11.Display;
+import com.sun.jna.platform.unix.X11.XClientMessageEvent;
+import com.sun.jna.platform.unix.X11.XEvent;
+
+public class Fullscreen
+{
+
+    private static final int _NET_WM_STATE_REMOVE = 0;
+    private static final int _NET_WM_STATE_ADD = 1;
+
+    public static void setFullScreenWindow(Window window, boolean fullScreen)
+    {
+        String osName = System.getProperty("os.name");
+        if (osName.equals("Linux"))
+        {
+            setFullScreenWindowLinux(window, fullScreen);
+        }
+        else
+        {
+            setFullScreenWindowLightweight(window, fullScreen);
+        }
+    }
+
+    private static boolean setFullScreenWindowLightweight(Window window, boolean fullScreen)
+    {
+        throw new UnsupportedOperationException("I should get a windows / OS X machine to test this.");
+    }
+
+    private static void setFullScreenWindowLinux(Window window, boolean fullScreen)
+    {
+        X11 x11Instance = X11.INSTANCE;
+        Display display = null;
+        try
+        {
+            display = x11Instance.XOpenDisplay(null);
+            long windowID = Native.getWindowID(window);
+
+            // Event set up
+            XEvent xEvent = new XEvent();
+            xEvent.setType(XClientMessageEvent.class);
+
+            xEvent.xclient.type = X11.ClientMessage;
+            xEvent.xclient.message_type = x11Instance.XInternAtom(display, "_NET_WM_STATE", false);
+            xEvent.xclient.format = 32;
+            xEvent.xclient.window = new com.sun.jna.platform.unix.X11.Window(windowID);
+            xEvent.xclient.send_event = 1;
+            xEvent.xclient.serial = new NativeLong(0L);
+
+            xEvent.xclient.data.setType(NativeLong[].class);
+            xEvent.xclient.data.l[0] = new NativeLong(fullScreen ? _NET_WM_STATE_ADD : _NET_WM_STATE_REMOVE);
+            xEvent.xclient.data.l[1] = x11Instance.XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", false);
+            xEvent.xclient.data.l[2] = new NativeLong(0L);
+            xEvent.xclient.data.l[3] = new NativeLong(0L);
+            xEvent.xclient.data.l[4] = new NativeLong(0L);
+
+            // Make it so
+            NativeLong mask = new NativeLong(X11.SubstructureRedirectMask | X11.SubstructureNotifyMask);
+            x11Instance.XSendEvent(display, x11Instance.XDefaultRootWindow(display), 0, mask, xEvent);
+            x11Instance.XFlush(display);
+        }
+        finally
+        {
+            if (display != null)
+            {
+                x11Instance.XCloseDisplay(display);
+            }
+        }
+    }
+}
