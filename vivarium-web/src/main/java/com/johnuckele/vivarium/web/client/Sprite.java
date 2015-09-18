e7bbe7692f2da2229bc2ba836e142e3ed5fd1d82
John H. Uckele <j@vivarium.io>
2015-09-17 22:21:51 -0400

    Vivarium-web dropped
    I've decided to drop vivarium web due to extensive contortions caused by
    the lack of support for any serious reflection in GWT. My needs to run the
    visualizer are minor, and while a plugin-free web based visualizer is a
    nice to have feature, I would rather focus on being able to iterate quickly
    on genetic algorithm or neurology code.
    
    I decided not to drop vivarium-web unless I could build a reasonable
    replacement first, and that is the culmination of this branch. This branch
    introduced vivarium-swing, a Java application, swing based, animated
    visualizer. Early versions of this were visually identical to vivarium-web,
    but tend to run much faster. Moving forward, there are a lot of visualizer
    improvements that can be made, and vivarium-swing will eventually far
    exceed vivarium-web in functionality.

--- vivarium-web/src/main/java/com/johnuckele/vivarium/web/client/Sprite.java
+++
@@ -1,8 +0,0 @@
-package com.johnuckele.vivarium.web.client;
-
-public enum Sprite
-{
-	FLOOR, WALL, FOOD,
-	RED_CREATURE_1, RED_CREATURE_2, RED_CREATURE_3,
-	BLUE_CREATURE_1, BLUE_CREATURE_2, BLUE_CREATURE_3;
-}
