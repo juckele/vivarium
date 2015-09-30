21d44bba3dc74be4f4d40b9775757c379fcfac0f
John H. Uckele <j@vivarium.io>
2015-09-29 22:39:50 -0400

    Basic hooks for web socket client worker and server

---
+++ vivarium-server/src/main/java/io/vivarium/client/Worker.java
@@ -0,0 +1,53 @@
+package io.vivarium.client;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class Worker extends WebSocketClient
+{
+    public Worker() throws URISyntaxException
+    {
+        super(new URI("http", null, "localhost", 13731, "/", null, null));
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata)
+    {
+        System.err.println("Shake it Open " + handshakedata);
+    }
+
+    @Override
+    public void onMessage(String message)
+    {
+        System.err.println("Message the Message " + message);
+
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote)
+    {
+        System.err.println("Close it down " + code + " / " + reason + " + " + remote);
+    }
+
+    @Override
+    public void onError(Exception ex)
+    {
+        System.err.println("ERROR " + ex);
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            Worker worker = new Worker();
+
+        }
+        catch (URISyntaxException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- vivarium-server/src/main/java/io/vivarium/server/Server.java
+++ vivarium-server/src/main/java/io/vivarium/server/Server.java
@@ -1,6 +1,70 @@
 package io.vivarium.server;
 
-public class Server
+import java.net.InetSocketAddress;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import io.vivarium.client.Worker;
+
+public class Server extends WebSocketServer
 {
+    public static InetSocketAddress PORT = new InetSocketAddress(13731);
+
+    public Server() throws UnknownHostException
+    {
+        super(PORT);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake)
+    {
+        System.out.println("Web Socket Connection Oopened. " + conn + " ~ " + handshake);
+
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote)
+    {
+        System.out.println("Web Socket Connection closed. " + conn + " ~ " + code + " # " + reason + " & " + remote);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message)
+    {
+        System.out.println("Web Socket Message . " + conn + " ~ " + message);
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex)
+    {
+        System.out.println("Web Socket Error . " + conn + " ~ " + ex);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Running Vivarium Research Server.");
+        try
+        {
+            Server server = new Server();
+            server.start();
+        }
+        catch (UnknownHostException e1)
+        {
+            e1.printStackTrace();
+        }
+        try
+        {
+            Worker worker = new Worker();
+            worker.connect();
+        }
+        catch (URISyntaxException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
 }
