b6182d06587635605bcc12425ea7863daf9b5355
John H. Uckele <j@vivarium.io>
2015-10-06 20:06:46 -0400

    Added additional tests for simulation running scripts

--- vivarium-scripts/src/test/java/io/vivarium/scripts/RunSimulationTest.java
+++ vivarium-scripts/src/test/java/io/vivarium/scripts/RunSimulationTest.java
@@ -8,6 +8,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import com.johnuckele.vtest.Tester;
+
 public class RunSimulationTest
 {
     @Rule
@@ -34,6 +36,23 @@ public class RunSimulationTest
     }
 
     @Test
+    public void testTimeLimit()
+    {
+        {
+            String[] commandArgs = { "-o", path + "w.viv" };
+            CreateWorld.main(commandArgs);
+        }
+        long start = System.currentTimeMillis();
+        {
+            String[] commandArgs = { "-i", path + "w.viv", "-o", path + "w2.viv", "-s", "1" };
+            RunSimulation.main(commandArgs);
+        }
+        long end = System.currentTimeMillis();
+        Tester.greaterOrEqual("World Simulation should take at least one second", (end - start) / 1000.0, 1, 0.0);
+        Tester.lessOrEqual("World Simulation should be less than two seconds", (end - start) / 1000.0, 2, 0.0);
+    }
+
+    @Test
     public void testDefaultWithSimulation()
     {
         {
@@ -44,5 +63,21 @@ public class RunSimulationTest
             String[] commandArgs = { "-i", path + "s.viv", "-o", path + "s2.viv" };
             RunSimulation.main(commandArgs);
         }
+    }
+
+    @Test
+    public void testTimeLimitAndTickLimit()
+    {
+        {
+            String[] commandArgs = { "-o", path + "s.viv", "-t", "1" };
+            CreateSimulation.main(commandArgs);
+        }
+        long start = System.currentTimeMillis();
+        {
+            String[] commandArgs = { "-i", path + "s.viv", "-o", path + "s2.viv", "-s", "1" };
+            RunSimulation.main(commandArgs);
+        }
+        long end = System.currentTimeMillis();
+        Tester.lessThan("World Simulation should take less than a second", (end - start) / 1000.0, 1, 0.0);
     }
 }
