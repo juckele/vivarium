b16844789f00d141d1a2854c5dfafef0f25d64b1
John H. Uckele <j@vivarium.io>
2015-10-27 12:43:26 -0400

    [49] Benchmark script.
    This script measures how many creature ticks per second can be performed
    by a computer at various thread counts. Designed for finding optimal
    thread count for system throughput.

---
+++ vivarium-scripts/src/main/java/io/vivarium/scripts/RunBenchmark.java
@@ -0,0 +1,156 @@
+package io.vivarium.scripts;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+
+import io.vivarium.core.Blueprint;
+import io.vivarium.core.EntityType;
+import io.vivarium.core.World;
+import io.vivarium.core.simulation.Simulation;
+
+public class RunBenchmark extends CommonsScript
+{
+    public RunBenchmark(String[] args)
+    {
+        super(args);
+    }
+
+    @Override
+    protected List<Option> getScriptSpecificOptions()
+    {
+        LinkedList<Option> options = new LinkedList<Option>();
+        return options;
+    }
+
+    @Override
+    protected String getExtraArgString()
+    {
+        return "";
+    }
+
+    @Override
+    protected String getUsageHeader()
+    {
+        return "Run a benchmark to determine a computers throughput for Vivarium simulations.";
+    }
+
+    @Override
+    protected void run(CommandLine commandLine)
+    {
+        try
+        {
+            System.out.println("Running single thread benchmark:");
+            // Do this just to give the JIT Compiler some stuff to optimize
+            inlineTest(100);
+
+            int iterations = 1000;
+            int size = 10;
+            double result;
+            System.out.println("Inline:");
+            result = iterateTest(iterations, size);
+            System.out.println(String.format("%sx%s:\n Average: %4.3e\n\n", size, size, result));
+
+            System.out.println("Threads");
+            for (int threadCount = 1; threadCount <= 16; threadCount++)
+            {
+                size = 100;
+                result = threadTest(threadCount, iterations, size);
+                System.out.println(
+                        String.format("%sx%s (%s threads):\n Average: %4.3e\n\n", size, size, threadCount, result));
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static double threadTest(int threadPoolSize, int iterationsPerThread, int size) throws InterruptedException
+    {
+        double total = 0;
+        BenchmarkThread[] threads = new BenchmarkThread[threadPoolSize];
+        for (int i = 0; i < threadPoolSize; i++)
+        {
+            threads[i] = new BenchmarkThread(iterationsPerThread, size);
+        }
+        for (int i = 0; i < threadPoolSize; i++)
+        {
+            threads[i].start();
+        }
+        for (int i = 0; i < threadPoolSize; i++)
+        {
+            threads[i].join();
+            total += threads[i].getResult();
+        }
+        return total;
+    }
+
+    private static class BenchmarkThread extends Thread
+    {
+        private final int _iterations;
+        private final int _size;
+        private double result = 0;
+
+        public BenchmarkThread(int iterations, int size)
+        {
+            _iterations = iterations;
+            _size = size;
+        }
+
+        public double getResult()
+        {
+            return result;
+        }
+
+        @Override
+        public void run()
+        {
+            result = iterateTest(_iterations, _size);
+        }
+    }
+
+    private static double iterateTest(int iterations, int size)
+    {
+        // Tests, by size
+        double min = Double.MAX_VALUE;
+        double max = 0;
+        double total = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            double testRun = inlineTest(25);
+            min = Math.min(min, testRun);
+            max = Math.max(max, testRun);
+            total += testRun;
+            // System.out.print(String.format("%4.3e ", testRun));
+        }
+        double average = total / iterations;
+        return average;
+    }
+
+    private static double inlineTest(int size)
+    {
+        Blueprint blueprint = Blueprint.makeDefault();
+        blueprint.setSize(size);
+        blueprint.getSpecies().get(0).setMaximumFood(Integer.MAX_VALUE); // Increase max food to prevent creatures from
+                                                                         // starving during the benchmark
+        World world = new World(blueprint);
+        int tickCount = 2000;
+        int worldPopulation = world.getCount(EntityType.CREATURE);
+        long startTime = System.currentTimeMillis();
+        Simulation.runForUpTo(world, tickCount);
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        double creatureTicksPerSeconds = worldPopulation * tickCount / (totalTime / 1000.0);
+        return creatureTicksPerSeconds;
+    }
+
+    public static void main(String[] args)
+    {
+        new RunBenchmark(args);
+    }
+
+}
