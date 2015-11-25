c3df6141077b688a7de09609fd3fbb37ffcec4df
John H. Uckele <j@vivarium.io>
2015-11-24 23:03:48 -0500

    Revert "Updated upsert utility to use prepared statements."
    This reverts commit 1ebdd3764db498f5a5293d5b168be7b254cff09b.
    
    The prepared statement JDBC code seems to have a difficult time
    dealing with parsing UUID values that have been sent via string.
    
    Since I'm not worried about SQL injection or performance at this
    point, I'm reverting this change.

--- vivarium-db/src/main/java/io/vivarium/db/DatabaseUtils.java
+++ vivarium-db/src/main/java/io/vivarium/db/DatabaseUtils.java
@@ -6,8 +6,8 @@ package io.vivarium.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -30,115 +30,86 @@ public class DatabaseUtils
     public static void upsert(Connection connection, String tableName, Map<String, Object> relation,
             List<String> keyColumns) throws SQLException
     {
-        boolean originalAutoCommit = connection.getAutoCommit();
-        connection.setAutoCommit(false);
-        innerUpsert(connection, tableName, relation, keyColumns);
-        connection.commit();
-        connection.setAutoCommit(originalAutoCommit);
+        try (Statement sqlStatement = connection.createStatement();)
+        {
+            // Build lists for all columns and non-key columns for streaming over while we build the the SQL statements.
+            List<String> allColumns = new LinkedList<String>();
+            allColumns.addAll(relation.keySet());
+            List<String> nonKeyColumns = new LinkedList<String>();
+            for (String columnName : relation.keySet())
+            {
+                if (!keyColumns.contains(columnName))
+                {
+                    nonKeyColumns.add(columnName);
+                }
+            }
+
+            // The values to actually use in the SQL. SQL null is fine for null values, and we can call toString() on
+            // primitives directly, but String based values need to be wrapped with single quotes for sql.
+            Map<String, String> sqlStrings = new HashMap<String, String>();
+            for (String columnName : allColumns)
+            {
+                Object columnValue = relation.get(columnName);
+                if (columnValue == null)
+                {
+                    sqlStrings.put(columnName, "null");
+                }
+                else if (Reflection.isPrimitive(columnValue.getClass()))
+                {
+                    sqlStrings.put(columnName, relation.get(columnName).toString());
+                }
+                else
+                {
+                    sqlStrings.put(columnName, '\'' + relation.get(columnName).toString() + '\'');
+                }
+            }
+
+            // Builds an update string in the form
+            // UPDATE table
+            // SET key1=value1, key2=value2
+            // WHERE id_key = id_value;
+            StringBuilder updateStringBuilder = new StringBuilder();
+            updateStringBuilder.append("UPDATE ");
+            updateStringBuilder.append(tableName);
+            updateStringBuilder.append(" SET ");
+            updateStringBuilder.append(Joiner.on(", ")
+                    .join(nonKeyColumns.stream().map(i -> String.format("%s=%s", i, sqlStrings.get(i))).iterator()));
+            updateStringBuilder.append(" WHERE ");
+            updateStringBuilder.append(Joiner.on(", ")
+                    .join(keyColumns.stream().map(i -> String.format("%s=%s", i, sqlStrings.get(i))).iterator()));
+            updateStringBuilder.append(";");
+
+            // Builds an insert string in the form
+            // INSERT INTO table (key1, key2)
+            // SELECT value1, value2
+            // WHERE NOT EXISTS (SELECT 1 FROM table WHERE id_key = id_value);
+            StringBuilder insertStringBuilder = new StringBuilder();
+            insertStringBuilder.append("INSERT INTO ");
+            insertStringBuilder.append(tableName);
+            insertStringBuilder.append(" (");
+            insertStringBuilder.append(Joiner.on(", ").join(allColumns));
+            insertStringBuilder.append(") SELECT ");
+            insertStringBuilder.append(Joiner.on(", ")
+                    .join(nonKeyColumns.stream().map(i -> String.format("%s", sqlStrings.get(i))).iterator()));
+            insertStringBuilder.append(" WHERE NOT EXISTS (SELECT 1 FROM ");
+            insertStringBuilder.append(tableName);
+            insertStringBuilder.append(" WHERE ");
+            insertStringBuilder.append(Joiner.on(", ")
+                    .join(keyColumns.stream().map(i -> String.format("%s=%s", i, sqlStrings.get(i))).iterator()));
+            insertStringBuilder.append(");");
+
+            // Run the upsert statements.
+            sqlStatement.execute(updateStringBuilder.toString());
+            sqlStatement.execute(insertStringBuilder.toString());
+        }
     }
 
     public static void upsert(Connection connection, String tableName, List<Map<String, Object>> relations,
             List<String> keyColumns) throws SQLException
     {
-        boolean originalAutoCommit = connection.getAutoCommit();
-        connection.setAutoCommit(false);
         for (Map<String, Object> relation : relations)
         {
-            innerUpsert(connection, tableName, relation, keyColumns);
-        }
-        connection.commit();
-        connection.setAutoCommit(originalAutoCommit);
-    }
-
-    private static void innerUpsert(Connection connection, String tableName, Map<String, Object> relation,
-            List<String> keyColumns) throws SQLException
-    {
-        // Build lists for all columns and non-key columns for streaming over while we build the the SQL statements.
-        List<String> allColumns = new LinkedList<String>();
-        allColumns.addAll(relation.keySet());
-        List<String> nonKeyColumns = new LinkedList<String>();
-        for (String columnName : relation.keySet())
-        {
-            if (!keyColumns.contains(columnName))
-            {
-                nonKeyColumns.add(columnName);
-            }
-        }
-
-        // The values to actually use in the SQL. SQL null is fine for null values, and we can call toString() on
-        // primitives directly, but String based values need to be wrapped with single quotes for sql.
-        Map<String, String> sqlStrings = new HashMap<String, String>();
-        for (String columnName : allColumns)
-        {
-            Object columnValue = relation.get(columnName);
-            if (columnValue == null)
-            {
-                sqlStrings.put(columnName, "null");
-            }
-            else if (Reflection.isPrimitive(columnValue.getClass()))
-            {
-                sqlStrings.put(columnName, relation.get(columnName).toString());
-            }
-            else
-            {
-                sqlStrings.put(columnName, '\'' + relation.get(columnName).toString() + '\'');
-            }
-        }
-
-        // Builds an update string in the form
-        // UPDATE table
-        // SET key1=value1, key2=value2
-        // WHERE id_key = id_value;
-        StringBuilder updateStringBuilder = new StringBuilder();
-        updateStringBuilder.append("UPDATE ");
-        updateStringBuilder.append(tableName);
-        updateStringBuilder.append(" SET ");
-        updateStringBuilder
-                .append(Joiner.on(", ").join(nonKeyColumns.stream().map(i -> String.format("%s=?", i)).iterator()));
-        updateStringBuilder.append(" WHERE ");
-        updateStringBuilder
-                .append(Joiner.on(", ").join(keyColumns.stream().map(i -> String.format("%s=?", i)).iterator()));
-        updateStringBuilder.append(";");
-
-        // Builds an insert string in the form
-        // INSERT INTO table (key1, key2)
-        // SELECT value1, value2
-        // WHERE NOT EXISTS (SELECT 1 FROM table WHERE id_key = id_value);
-        StringBuilder insertStringBuilder = new StringBuilder();
-        insertStringBuilder.append("INSERT INTO ");
-        insertStringBuilder.append(tableName);
-        insertStringBuilder.append(" (");
-        insertStringBuilder.append(Joiner.on(", ").join(allColumns));
-        insertStringBuilder.append(") SELECT ");
-        insertStringBuilder.append(Joiner.on(", ").join(nonKeyColumns.stream().map(i -> "?").iterator()));
-        insertStringBuilder.append(" WHERE NOT EXISTS (SELECT 1 FROM ");
-        insertStringBuilder.append(tableName);
-        insertStringBuilder.append(" WHERE ");
-        insertStringBuilder.append(Joiner.on(", ")
-                .join(keyColumns.stream().map(i -> String.format("%s=?", i, sqlStrings.get(i))).iterator()));
-        insertStringBuilder.append(");");
-
-        try (PreparedStatement updateStatement = connection.prepareStatement(updateStringBuilder.toString());
-                PreparedStatement insertStatement = connection.prepareStatement(insertStringBuilder.toString());)
-        {
-            // Bind all of the variables
-            int parameterIndex = 1;
-            for (String columnName : nonKeyColumns)
-            {
-                updateStatement.setObject(parameterIndex, sqlStrings.get(columnName));
-                insertStatement.setObject(parameterIndex, sqlStrings.get(columnName));
-                parameterIndex++;
-            }
-            for (String columnName : keyColumns)
-            {
-                updateStatement.setObject(parameterIndex, sqlStrings.get(columnName));
-                insertStatement.setObject(parameterIndex, sqlStrings.get(columnName));
-                parameterIndex++;
-            }
-
-            // Run the upsert statements.
-            updateStatement.executeUpdate();
-            insertStatement.executeUpdate();
+            upsert(connection, tableName, relation, keyColumns);
         }
     }
 }
