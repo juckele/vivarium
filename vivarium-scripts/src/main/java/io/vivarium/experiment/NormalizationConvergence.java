1fb85b78cd986f3f1f6f701cf29f30c27ea21d57
John H. Uckele <j@vivarium.io>
2015-12-16 17:11:34 -0500

    Removed extraneous TODO

--- vivarium-scripts/src/main/java/io/vivarium/experiment/NormalizationConvergence.java
+++ vivarium-scripts/src/main/java/io/vivarium/experiment/NormalizationConvergence.java
@@ -145,7 +145,6 @@ public class NormalizationConvergence
         }
         catch (InterruptedException e)
         {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
