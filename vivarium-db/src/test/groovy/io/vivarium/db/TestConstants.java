a9764cb3f05cc9e5fc60f28981f9274a6cc70238
John H. Uckele <j@vivarium.io>
2015-12-17 17:17:34 -0500

    Basic DB tests

---
+++ vivarium-db/src/test/groovy/io/vivarium/db/TestConstants.java
@@ -0,0 +1,8 @@
+package io.vivarium.db;
+
+public class TestConstants
+{
+    public static final String TEST_DATABASE_NAME = "test";
+    public static final String TEST_DATABASE_PASSWORD = "test";
+    public static final String TEST_DATABASE_USER = "test";
+}
---
+++ vivarium-db/src/test/groovy/io/vivarium/persistence/JobPersistenceTest.java
@@ -0,0 +1,19 @@
+package io.vivarium.persistence;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import io.vivarium.db.DatabaseUtils;
+import io.vivarium.db.TestConstants;
+
+public class JobPersistenceTest
+{
+    @Test
+    public void testUpdateJobStatuses() throws SQLException
+    {
+        Connection databaseConnection = DatabaseUtils.createDatabaseConnection(TestConstants.TEST_DATABASE_NAME,
+                TestConstants.TEST_DATABASE_USER, TestConstants.TEST_DATABASE_PASSWORD);
+    }
+}
