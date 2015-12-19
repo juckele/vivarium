6bfa91e22f62dd0c708e8eef27ac5d193bfcd65d
John H. Uckele <j@vivarium.io>
2015-12-18 23:47:11 -0500

    Added some basic ClientConnectionManager tests

---
+++ vivarium-ws/src/test/java/io/vivarium/server/ClientConnectionManagerTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2015 John H Uckele. All rights reserved.
+ */
+
+package io.vivarium.server;
+
+import static org.mockito.Mockito.mock;
+
+import org.java_websocket.WebSocket;
+import org.junit.Test;
+
+import com.johnuckele.vtest.Tester;
+
+import io.vivarium.util.UUID;
+
+public class ClientConnectionManagerTest
+{
+    @Test
+    public void testRegisterWorker()
+    {
+        // Create CCM
+        ClientConnectionManager manager = new ClientConnectionManager();
+
+        // Register a worker
+        UUID workerID = UUID.randomUUID();
+        WebSocket workerSocket = mock(WebSocket.class);
+        manager.registerWorker(workerID, workerSocket);
+
+        // Get the worker socket
+        WebSocket fetchedWorkerSocket = manager.getSocketForWorker(workerID).get();
+
+        Tester.isTrue("original socket and fetched socket are the same", workerSocket == fetchedWorkerSocket);
+
+        // Register a second worker
+        UUID workerID2 = UUID.randomUUID();
+        WebSocket workerSocket2 = mock(WebSocket.class);
+        manager.registerWorker(workerID2, workerSocket2);
+
+        // Get the second worker socket
+        WebSocket fetchedWorkerSocket2 = manager.getSocketForWorker(workerID2).get();
+        Tester.isFalse("Fetched sockets from different workers are not the same",
+                fetchedWorkerSocket == fetchedWorkerSocket2);
+    }
+
+}
