6baeaec85252eba4885047110be08082c352b845
John H. Uckele <j@vivarium.io>
2015-12-18 23:32:39 -0500

    Added requeue test for the VoidFunctionSchedulerTest

--- vivarium-ws/src/test/java/io/vivarium/server/VoidFunctionSchedulerTest.java
+++ vivarium-ws/src/test/java/io/vivarium/server/VoidFunctionSchedulerTest.java
@@ -15,12 +15,40 @@ public class VoidFunctionSchedulerTest
         Tester.equal("Initial execute count is zero: ", function.getExecuteCount(), 0);
 
         scheduler.start();
-        Thread.sleep(1);
+        Thread.sleep(2);
         Tester.equal("After start, count increments: ", function.getExecuteCount(), 1);
 
         scheduler.execute();
-        Thread.sleep(1);
+        Thread.sleep(2);
         Tester.equal("After execution, count increments: ", function.getExecuteCount(), 2);
+
+        scheduler.stop();
+    }
+
+    @Test
+    public void testManualExecuteQueue() throws InterruptedException
+    {
+        SlowIncrementer function = new SlowIncrementer(4);
+        VoidFunctionScheduler scheduler = new VoidFunctionScheduler(function, Long.MAX_VALUE);
+
+        Tester.equal("Initial execute count is zero: ", function.getExecuteCount(), 0);
+
+        scheduler.start();
+        Thread.sleep(10);
+        Tester.equal("After start, count increments: ", function.getExecuteCount(), 1);
+
+        scheduler.execute();
+        scheduler.execute();
+        Tester.equal("Neither execute has been counted yet: ", function.getExecuteCount(), 1);
+        Thread.sleep(10);
+        Tester.equal("But both will be completed as soon as possible: ", function.getExecuteCount(), 3);
+
+        scheduler.execute();
+        scheduler.execute();
+        scheduler.execute();
+        Thread.sleep(10);
+        Tester.equal("The queue only stores a single extra execute though, additionals are lost: ",
+                function.getExecuteCount(), 5);
 
         scheduler.stop();
     }
@@ -42,17 +70,47 @@ public class VoidFunctionSchedulerTest
 
     private static class Incrementer implements VoidFunction
     {
-        int executeCount = 0;
+        int _executeCount = 0;
 
         @Override
         public synchronized void execute()
         {
-            executeCount++;
+            _executeCount++;
         }
 
         public int getExecuteCount()
         {
-            return executeCount;
+            return _executeCount;
+        }
+    }
+
+    private static class SlowIncrementer implements VoidFunction
+    {
+        int _executeCount = 0;
+        final long _delay;
+
+        public SlowIncrementer(long delay)
+        {
+            this._delay = delay;
+        }
+
+        @Override
+        public synchronized void execute()
+        {
+            try
+            {
+                Thread.sleep(_delay);
+                _executeCount++;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        public int getExecuteCount()
+        {
+            return _executeCount;
         }
     }
 }
