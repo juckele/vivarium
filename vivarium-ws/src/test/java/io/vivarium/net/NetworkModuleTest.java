95a50272845d250ed5ba7dd73e90782e61f3b413
John H. Uckele <j@vivarium.io>
2015-12-30 08:29:26 -0500

    Added addRemove and remove without pre-existing add tests

--- vivarium-ws/src/test/java/io/vivarium/net/NetworkModuleTest.java
+++ vivarium-ws/src/test/java/io/vivarium/net/NetworkModuleTest.java
@@ -3,6 +3,7 @@ package io.vivarium.net;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
@@ -12,6 +13,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.johnuckele.vtest.Tester;
 
 import io.vivarium.net.messages.Message;
 import io.vivarium.util.UUID;
@@ -40,6 +42,61 @@ public class NetworkModuleTest
         verify(listener).onMessage(eq(null), argument.capture());
         assertEquals(message.getClass(), argument.getValue().getClass());
         assertEquals(message.getMessageID(), argument.getValue().getMessageID());
+
+        Tester.pass("The above code should execute without problems.");
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testAddRemoveMessageListener() throws Exception
+    {
+        // Build a network module
+        InboundNetworkListener inboundNetworkListener = mock(InboundNetworkListener.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        NetworkModule networkModule = new ServerNetworkModule(inboundNetworkListener, objectMapper);
+
+        // Build and set up the message listener
+        Message message = new TestMessage(UUID.randomUUID());
+        MessageListener<TestMessage> listener = mock(MessageListener.class);
+        networkModule.addMessageListener(listener, TestMessage.class);
+
+        // Remove the message listener
+        networkModule.removeMessageListener(listener, TestMessage.class);
+
+        // Send the message to the network module
+        networkModule.onMessage(null, objectMapper.writeValueAsString(message));
+
+        // Verify that the listener was not called
+        ArgumentCaptor<Message> argument = ArgumentCaptor.forClass(Message.class);
+        verify(listener, never()).onMessage(eq(null), argument.capture());
+
+        Tester.pass("The above code should execute without problems.");
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testRemoveMessageListener() throws Exception
+    {
+        // Build a network module
+        InboundNetworkListener inboundNetworkListener = mock(InboundNetworkListener.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        NetworkModule networkModule = new ServerNetworkModule(inboundNetworkListener, objectMapper);
+
+        // Build the message listener
+        Message message = new TestMessage(UUID.randomUUID());
+        MessageListener<TestMessage> listener = mock(MessageListener.class);
+
+        // Remove the message listener without adding it
+        networkModule.removeMessageListener(listener, TestMessage.class);
+
+        // Send the message to the network module
+        networkModule.onMessage(null, objectMapper.writeValueAsString(message));
+
+        // Verify that the listener was not called
+        ArgumentCaptor<Message> argument = ArgumentCaptor.forClass(Message.class);
+        verify(listener, never()).onMessage(eq(null), argument.capture());
+
+        Tester.pass("The above code should execute without problems.");
     }
 
     private static class TestMessage extends Message
