2e25a9603331c6533da957a3fcde56c45677bd8a
John H. Uckele <j@vivarium.io>
2015-12-30 08:36:21 -0500

    Test of MessageListener super/subclass resolution
    -Superclass listeners should be called on all subclass messages.
    -Subclass listeners should not be called on superclass messages.

--- vivarium-ws/src/test/java/io/vivarium/net/NetworkModuleTest.java
+++ vivarium-ws/src/test/java/io/vivarium/net/NetworkModuleTest.java
@@ -99,10 +99,70 @@ public class NetworkModuleTest
         Tester.pass("The above code should execute without problems.");
     }
 
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testSubclassListenerDoesNotGetCalled() throws Exception
+    {
+        // Build a network module
+        InboundNetworkListener inboundNetworkListener = mock(InboundNetworkListener.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        NetworkModule networkModule = new ServerNetworkModule(inboundNetworkListener, objectMapper);
+
+        // Build and set up the message listener
+        MessageListener<AdvancedTestMessage> listener = mock(MessageListener.class);
+        networkModule.addMessageListener(listener, AdvancedTestMessage.class);
+
+        // Send the message to the network module
+        Message message = new TestMessage(UUID.randomUUID());
+        networkModule.onMessage(null, objectMapper.writeValueAsString(message));
+
+        // Verify that the listener was not called
+        ArgumentCaptor<Message> argument = ArgumentCaptor.forClass(Message.class);
+        verify(listener, never()).onMessage(eq(null), argument.capture());
+
+        Tester.pass("The above code should execute without problems.");
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testSuperclassListenerDoesGetCalled() throws Exception
+    {
+        // Build a network module
+        InboundNetworkListener inboundNetworkListener = mock(InboundNetworkListener.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        NetworkModule networkModule = new ServerNetworkModule(inboundNetworkListener, objectMapper);
+
+        // Build and set up the message listener
+        MessageListener<TestMessage> listener = mock(MessageListener.class);
+        networkModule.addMessageListener(listener, TestMessage.class);
+
+        // Send the message to the network module
+        AdvancedTestMessage message = new AdvancedTestMessage(UUID.randomUUID());
+        networkModule.onMessage(null, objectMapper.writeValueAsString(message));
+
+        // Verify that the listener was called
+        ArgumentCaptor<Message> argument = ArgumentCaptor.forClass(Message.class);
+        verify(listener).onMessage(eq(null), argument.capture());
+        assertEquals(message.getClass(), argument.getValue().getClass());
+        assertEquals(message.getMessageID(), argument.getValue().getMessageID());
+
+        Tester.pass("The above code should execute without problems.");
+    }
+
     private static class TestMessage extends Message
     {
         @JsonCreator
         protected TestMessage(@JsonProperty("messageID") @JsonSerialize(using = UUIDSerializer.class) UUID messageID)
+        {
+            super(messageID);
+        }
+    }
+
+    private static class AdvancedTestMessage extends TestMessage
+    {
+        @JsonCreator
+        protected AdvancedTestMessage(
+                @JsonProperty("messageID") @JsonSerialize(using = UUIDSerializer.class) UUID messageID)
         {
             super(messageID);
         }
