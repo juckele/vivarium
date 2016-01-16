28ac7bb16650dce9a256291392f6b49fbda6696b
John H. Uckele <j@vivarium.io>
2016-01-16 08:51:54 -0800

    Deleted unused code in BrainDemo

--- vivarium-web/src/main/java/io/vivarium/web/client/BrainDemo.java
+++ vivarium-web/src/main/java/io/vivarium/web/client/BrainDemo.java
@@ -4,12 +4,7 @@
 
 package io.vivarium.web.client;
 
-import java.util.ArrayList;
-
-import javax.annotation.Nonnull;
-
 import org.realityforge.gwt.websockets.client.WebSocket;
-import org.realityforge.gwt.websockets.client.WebSocketListenerAdapter;
 
 import com.github.nmorel.gwtjackson.client.ObjectMapper;
 import com.google.gwt.animation.client.AnimationScheduler.AnimationCallback;
@@ -17,25 +12,16 @@ import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.canvas.dom.client.ImageData;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.shared.GWT;
 import com.google.gwt.dom.client.ImageElement;
 import com.google.gwt.event.dom.client.LoadEvent;
 import com.google.gwt.event.dom.client.LoadHandler;
-import com.google.gwt.event.logical.shared.CloseEvent;
-import com.google.gwt.event.logical.shared.CloseHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.googlecode.gwtstreamer.client.Streamer;
 
-import io.vivarium.core.Blueprint;
-import io.vivarium.core.Species;
-import io.vivarium.core.World;
+import io.vivarium.core.brain.Brain;
+import io.vivarium.core.brain.RandomBrain;
 import io.vivarium.net.messages.Message;
-import io.vivarium.net.messages.RequestResourceMessage;
-import io.vivarium.net.messages.ResourceFormat;
-import io.vivarium.net.messages.SendResourceMessage;
 import io.vivarium.serialization.VivariumObjectCollection;
 import io.vivarium.util.UUID;
 import io.vivarium.visualization.animation.Visualizer;
@@ -47,7 +33,7 @@ public class BrainDemo implements AnimationCallback, EntryPoint, LoadHandler
     private Canvas _canvas;
     private Canvas _tempCanvas;
     private Canvas _tempCanvas2;
-    private World world;
+    private Brain _brain;
     private VivariumObjectCollection _collection;
     // private GWTGraphics gwtGraphics;
     // private GWTScheduler gwtScheduler;
@@ -76,71 +62,14 @@ public class BrainDemo implements AnimationCallback, EntryPoint, LoadHandler
     @Override
     public void onModuleLoad()
     {
-        // Get options from parameters
-        String debug = Window.Location.getParameter(DEBUG_KEY);
-        if (debug != null && debug.toLowerCase().equals("true"))
-        {
-            _debug = true;
-        }
-        String tick = Window.Location.getParameter(TICK_KEY);
-        if (tick != null)
-        {
-            if (tick.toLowerCase().equals("perframe"))
-            {
-                _tickEveryFrame = true;
-                _ticksPerStep = 1;
-            }
-            else
-            {
-                try
-                {
-                    _ticksPerStep = Integer.parseInt(tick);
-                    _tickEveryFrame = true;
-                }
-                catch (NumberFormatException e)
-                {
-                    // Do nothing
-                }
-            }
-        }
-        String ws = Window.Location.getParameter(WEB_SOCKET_KEY);
-        if (ws != null)
-        {
-            if (ws.toLowerCase().equals("true"))
-            {
-                _resourceID = UUID.fromString("d51b6b31-84b5-0835-d5d5-05467ab4f04d");
-            }
-            else
-            {
-                _resourceID = UUID.fromString(ws);
-            }
-            String saveOnClose = Window.Location.getParameter(SAVE_ON_CLOSE);
-            if (saveOnClose != null)
-            {
-                _saveOnClose = true;
-                Window.alert("I want to save on close!");
-            }
-            startWS();
-        }
-        else
-        {
-            buildWorld();
-            setUpGraphics();
-            displayWorld();
-        }
+        buildBrain();
+        setUpGraphics();
+        displayWorld();
     }
 
-    private void buildWorld()
+    private void buildBrain()
     {
-        // Build the world
-        Blueprint blueprint = Blueprint.makeDefault();
-        Species s = Species.makeDefault();
-        ArrayList<Species> species = new ArrayList<Species>();
-        species.add(s);
-        blueprint.setSpecies(species);
-        blueprint.setSize(30);
-        blueprint.setWidth(45);
-        world = new World(blueprint);
+        _brain = new RandomBrain(6);
     }
 
     private void setUpGraphics()
@@ -149,68 +78,6 @@ public class BrainDemo implements AnimationCallback, EntryPoint, LoadHandler
         // gwtGraphics = new GWTGraphics(this);
         // gwtScheduler = new GWTScheduler(this, _tickEveryFrame, _ticksPerStep);
         // visualizer = new Visualizer(world, gwtGraphics, gwtScheduler);
-    }
-
-    private void startWS()
-    {
-        _mapper = GWT.create(MessageMapper.class);
-        // Pledge p = new Pledge(UUID.randomUUID());
-        // String encoding = _mapper.write(p);
-        // Window.alert(encoding);
-
-        _webSocket = WebSocket.newWebSocketIfSupported();
-        if (_webSocket != null)
-        {
-            _webSocket.setListener(new WebSocketListenerAdapter()
-            {
-                @Override
-                public void onOpen(@Nonnull final WebSocket webSocket)
-                {
-                    // After we have connected we can send
-                    RequestResourceMessage request = new RequestResourceMessage(_resourceID, ResourceFormat.GWT_STREAM);
-                    webSocket.send(_mapper.write(request));
-                }
-
-                @Override
-                public void onMessage(@Nonnull final WebSocket webSocket, @Nonnull final String data)
-                {
-                    Message incomingMessage = _mapper.read(data);
-                    if (incomingMessage instanceof SendResourceMessage)
-                    {
-                        SendResourceMessage sendResource = (SendResourceMessage) incomingMessage;
-                        _collection = (VivariumObjectCollection) Streamer.get()
-                                .fromString(sendResource.getDataString());
-
-                        // Finish loading
-                        world = _collection.getFirst(World.class);
-                        setUpGraphics();
-                        displayWorld();
-                    }
-                    // After we receive a message back we can close the socket
-                    // webSocket.close();
-                }
-            });
-            _webSocket.connect("ws://localhost:13731/");
-            if (_saveOnClose)
-            {
-                Window.addCloseHandler(new CloseHandler<Window>()
-                {
-                    @Override
-                    public void onClose(CloseEvent<Window> event)
-                    {
-                        Window.alert("Can I please save?");
-                        if (_webSocket != null && _webSocket.isConnected())
-                        {
-                            // After we have connected we can send
-                            SendResourceMessage sendMessage = new SendResourceMessage(_resourceID,
-                                    Streamer.get().toString(_collection), ResourceFormat.GWT_STREAM);
-                            _webSocket.send(_mapper.write(sendMessage));
-                            _webSocket.close();
-                        }
-                    }
-                });
-            }
-        }
     }
 
     private void displayWorld()
@@ -233,8 +100,8 @@ public class BrainDemo implements AnimationCallback, EntryPoint, LoadHandler
 
         // The canvas is our graphical space for all of the world display
         _canvas = Canvas.createIfSupported();
-        _canvas.setCoordinateSpaceWidth(world.getWorldWidth() * BrainDemo.PIXEL_BLOCK_SIZE);
-        _canvas.setCoordinateSpaceHeight(world.getWorldHeight() * BrainDemo.PIXEL_BLOCK_SIZE);
+        // _canvas.setCoordinateSpaceWidth(world.getWorldWidth() * BrainDemo.PIXEL_BLOCK_SIZE);
+        // _canvas.setCoordinateSpaceHeight(world.getWorldHeight() * BrainDemo.PIXEL_BLOCK_SIZE);
         RootPanel.get().add(_canvas);
 
         // All of the sprites are loaded from a single sprites image
