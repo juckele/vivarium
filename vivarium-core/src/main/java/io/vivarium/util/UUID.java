45086ff20e79cc0508fcbc07aa5762e19af081cb
John H. Uckele <j@vivarium.io>
2016-01-17 16:17:24 -0500

    Updated comment
    TODO: I still need to do some better analysis of the UUID and random code.

--- vivarium-core/src/main/java/io/vivarium/util/UUID.java
+++ vivarium-core/src/main/java/io/vivarium/util/UUID.java
@@ -53,7 +53,7 @@ public class UUID implements Serializable, Streamable
     public static UUID randomUUID()
     {
         // This is compatible with a real UUID, but it is not cryptographically secure. In an isolated environment, the
-        // cycle time of this UUID generator is only 2^64-1, but there are also 2^64 different possible cycles, so
+        // cycle time of this UUID generator is only 2^64-1, but there are also 2^64-1 different possible cycles, so
         // collisions should remain improbable.
         long long1 = (Rand.getInstance().getRandomLong()
                 | 0b00000000_00000000_00000000_00000000_00000000_00000000_01000000_00000000L)
