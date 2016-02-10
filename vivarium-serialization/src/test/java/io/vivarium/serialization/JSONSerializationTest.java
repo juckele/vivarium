2cb7fd6fd7675793fedf1870b397700515a32274
John H. Uckele <j@vivarium.io>
2016-02-10 18:37:15 -0500

    Switch comparison to use deep auto-generated comparisons.

--- vivarium-serialization/src/test/java/io/vivarium/serialization/JSONSerializationTest.java
+++ vivarium-serialization/src/test/java/io/vivarium/serialization/JSONSerializationTest.java
@@ -6,18 +6,13 @@ package io.vivarium.serialization;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.LinkedList;
-
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
-import com.johnuckele.vtest.Tester;
 
 import io.vivarium.core.Blueprint;
-import io.vivarium.core.Creature;
 import io.vivarium.core.Species;
 import io.vivarium.core.World;
-import io.vivarium.core.processor.NeuralNetwork;
 
 public class JSONSerializationTest
 {
@@ -38,39 +33,7 @@ public class JSONSerializationTest
         // Deserialize
         World deserializeWorld = JSONConverter.jsonStringToSerializerCollection(jsonString).getFirst(World.class);
 
-        // Deep compare of creature brains
-        LinkedList<Creature> originalCreatures = world.getCreatures();
-        LinkedList<Creature> deserializedCreatures = deserializeWorld.getCreatures();
-        Tester.equal("Creature counts should be the same", originalCreatures.size(), deserializedCreatures.size());
-        for (int index = 0; index < originalCreatures.size(); index++)
-        {
-            Creature originalCreature = originalCreatures.get(index);
-            Creature deserializedCreature = originalCreatures.get(index);
-            if (originalCreature.getProcessor() instanceof NeuralNetwork)
-            {
-                NeuralNetwork originalCreatureProcessor = (NeuralNetwork) originalCreature.getProcessor();
-                NeuralNetwork deserializedCreatureProcessor = (NeuralNetwork) deserializedCreature.getProcessor();
-                double[][][] originalCreatureProcessorWeights = originalCreatureProcessor.getWeights();
-                double[][][] deserializedCreatureProcessorWeights = deserializedCreatureProcessor.getWeights();
-                compareNeuralNetworkWeights(originalCreatureProcessorWeights, deserializedCreatureProcessorWeights);
-                assertEquals(Math.sqrt(42), originalCreatureProcessor.getGenomeLength(), 0.0000000001);
-                assertEquals(Math.sqrt(42), deserializedCreatureProcessor.getGenomeLength(), 0.0000000001);
-            }
-        }
-    }
-
-    private void compareNeuralNetworkWeights(double[][][] weights1, double[][][] weights2)
-    {
-        for (int i = 0; i < weights1.length; i++)
-        {
-            for (int j = 0; j < weights1[i].length; j++)
-            {
-                for (int k = 0; k < weights1[i][j].length; k++)
-                {
-                    assertEquals(weights1[i][j][k], weights2[i][j][k], 0);
-                }
-            }
-        }
-
+        // Deep compare of the worlds
+        assertEquals(world, deserializeWorld);
     }
 }
