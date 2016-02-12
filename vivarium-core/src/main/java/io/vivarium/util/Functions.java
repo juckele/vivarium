99817e839953ec64951ab7bb8efffd90b372d333
John H. Uckele <j@vivarium.io>
2016-02-11 23:38:19 -0500

    Improved runtime of generateDitherArray from N^3 to N.

--- vivarium-core/src/main/java/io/vivarium/util/Functions.java
+++ vivarium-core/src/main/java/io/vivarium/util/Functions.java
@@ -17,6 +17,9 @@
 
 package io.vivarium.util;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.google.common.base.Preconditions;
 
 public class Functions
@@ -54,8 +57,9 @@ public class Functions
      * In a grid search style application, this allows results to be generated at a low granularity, and over time to
      * generate additional higher granularity data points.
      *
-     * For example, the inputs of min = 1, max = 10, and steps = 10 will produce the output {1.0, 10.0, 5.0, 3.0, 7.0,
-     * 2.0, 4.0, 6.0, 8.0, 9.0}
+     * For example, the inputs of min = 1, max = 10, and steps = 10 will produce the output roughly equivalent to {1.0,
+     * 10.0, 5.0, 3.0, 7.0, 2.0, 4.0, 6.0, 8.0, 9.0}. The order of outputs is not guaranteed, but maximal gaps between
+     * preceeding values should always be observed.
      *
      * @param min
      *            The minimum value in the dither array. This will always be returned as the first element.
@@ -70,37 +74,59 @@ public class Functions
         Preconditions.checkArgument(steps >= 2);
         Preconditions.checkArgument(min < max);
 
+        // Build the basic data structures
         double[] ditherArray = new double[steps];
         double stepSize = (max - min) / (steps - 1);
+        int currentStepResolution = steps / 2;
 
+        // Immediately place the min value into the ditherArray and add it to the used values
+        Set<Integer> usedValues = new HashSet<>();
+        usedValues.add(0);
         ditherArray[0] = min;
         ditherArray[1] = max;
-        for (int outerIndex = 2; outerIndex < ditherArray.length; outerIndex++)
+
+        // Build the unusedValues, which should initially contain everything but value 0.
+        Set<Integer> unusedValues = new HashSet<>();
+        for (int i = 2; i < steps; i++)
         {
-            double targetValue = 0;
-            double maximumDifference = Double.MIN_VALUE;
-            for (int trialStep = 1; trialStep < steps; trialStep++)
+            unusedValues.add(i);
+        }
+
+        // Build the dither array, starting at the 3rd element (first and second element are always min and max)
+        int ditherArrayIndex = 2;
+        // While the resolution for steps is larger than 1, step based on step size from used values
+        while (currentStepResolution > 1)
+        {
+            // Track what we use for each pass based on step size, to move from unused to used values at the end
+            Set<Integer> newValues = new HashSet<>();
+
+            // For each used value, find the next value based on step size.
+            for (int usedValue : usedValues)
             {
-                double trialValue = min + trialStep * stepSize;
-                double minimumDifference = Double.MAX_VALUE;
-                for (int innerIndex = 0; innerIndex < outerIndex; innerIndex++)
-                {
-                    double difference = Math.abs(ditherArray[innerIndex] - trialValue);
-                    if (difference < minimumDifference)
-                    {
-                        minimumDifference = difference;
-                    }
-                }
-                if (minimumDifference > maximumDifference)
-                {
-                    targetValue = trialValue;
-                    maximumDifference = minimumDifference;
-                }
+                int newValue = usedValue + currentStepResolution;
+                newValues.add(newValue);
+                ditherArray[ditherArrayIndex++] = newValue * stepSize + min;
             }
-            ditherArray[outerIndex] = targetValue;
+
+            // After finding all new values, move all new values from unused to used values.
+            // Note: There is no need to check whether unusedValues contains any of the values we're seeing yet, because
+            // the step size shrinks faster than we can get collisions.
+            for (int newValue : newValues)
+            {
+                unusedValues.remove(newValue);
+                usedValues.add(newValue);
+            }
+
+            // At the end of the pass for this step size, halve the step size (rounding down).
+            currentStepResolution /= 2;
+        }
+        // After the step size resolution shrinks to 1, we just want to immediately go through all unused values and
+        // add them.
+        for (int unusedValue : unusedValues)
+        {
+            ditherArray[ditherArrayIndex++] = unusedValue * stepSize + min;
         }
 
         return ditherArray;
     }
-
 }
