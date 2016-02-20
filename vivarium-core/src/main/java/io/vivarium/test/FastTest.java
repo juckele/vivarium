25098fb599f38925097690968f48207b727e35e1
John H. Uckele <j@vivarium.io>
2016-02-20 06:07:48 -0500

    Set up test categories

---
+++ vivarium-core/src/main/java/io/vivarium/test/FastTest.java
@@ -0,0 +1,9 @@
+package io.vivarium.test;
+
+// In main source because I wanted test categories in sub-projects more
+// than I wanted to set up a test jar specific export from -core.
+// Feel free to move this package if you fix the build file too.
+public interface FastTest
+{
+
+}
