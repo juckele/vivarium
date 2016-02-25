67c46bdea97789cea75d563465f0f6a4558d6ee5
John H. Uckele <j@vivarium.io>
2016-02-25 09:36:33 -0500

    Removed redundant code

--- vivarium-ga/src/main/java/io/vivarium/ga/BehaviorBasedFitnessFunction.java
+++ vivarium-ga/src/main/java/io/vivarium/ga/BehaviorBasedFitnessFunction.java
@@ -1,9 +1,5 @@
 package io.vivarium.ga;
 
-import io.vivarium.core.Creature;
-
 public abstract class BehaviorBasedFitnessFunction extends FitnessFunction
 {
-    @Override
-    public abstract double evaluate(Creature c);
 }
--- vivarium-ga/src/main/java/io/vivarium/ga/SimulationBasedFitnessFunction.java
+++ vivarium-ga/src/main/java/io/vivarium/ga/SimulationBasedFitnessFunction.java
@@ -1,10 +1,5 @@
 package io.vivarium.ga;
 
-import io.vivarium.core.Creature;
-
 public abstract class SimulationBasedFitnessFunction extends FitnessFunction
 {
-
-    @Override
-    public abstract double evaluate(Creature c);
 }
