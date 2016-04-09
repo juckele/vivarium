038a54a485a2221abc0705d051a9e0f0aaba2f2b
John H. Uckele <j@vivarium.io>
2016-04-09 16:53:03 -0400

    Added HTML project which uses GWT to build the Visualizer

---
+++ vivarium-html/src/main/java/io/vivarium/client/HtmlLauncher.java
@@ -0,0 +1,23 @@
+package io.vivarium.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+import io.vivarium.visualizer.Vivarium;
+
+public class HtmlLauncher extends GwtApplication
+{
+
+    @Override
+    public GwtApplicationConfiguration getConfig()
+    {
+        return new GwtApplicationConfiguration(480, 320);
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener()
+    {
+        return new Vivarium();
+    }
+}
