b629b4b43713e7405791d71279be23132deff2b5
John H. Uckele <j@vivarium.io>
2016-04-09 17:17:06 -0400

    Experimental dynamic creature coloring

--- vivarium-visualizer/src/main/java/io/vivarium/visualizer/Vivarium.java
+++ vivarium-visualizer/src/main/java/io/vivarium/visualizer/Vivarium.java
@@ -4,18 +4,21 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Vivarium extends ApplicationAdapter
 {
     SpriteBatch batch;
     Texture img;
+    Sprite colored;
 
     @Override
     public void create()
     {
         batch = new SpriteBatch();
         img = new Texture("sprites.png");
+        colored = new Sprite(img);
     }
 
     @Override
@@ -23,8 +26,12 @@ public class Vivarium extends ApplicationAdapter
     {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        colored.setColor((float) Math.random(), (float) Math.random(), (float) Math.random(), 1f);
+        colored.setPosition(100, 00);
         batch.begin();
         batch.draw(img, 0, 0);
+        colored.draw(batch);
         batch.end();
     }
 }
