a7bf880eb2edf962855a08430634cb8f2a9edc78
John H. Uckele <j@vivarium.io>
2016-04-11 21:19:43 -0400

    Demo Buttons!

--- vivarium-visualizer/src/main/java/io/vivarium/visualizer/Vivarium.java
+++ vivarium-visualizer/src/main/java/io/vivarium/visualizer/Vivarium.java
@@ -4,8 +4,21 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import io.vivarium.core.Creature;
 import io.vivarium.core.Direction;
@@ -27,15 +40,87 @@ public class Vivarium extends ApplicationAdapter
     private SpriteBatch _batch;
     private Texture _img;
 
+    // High Level Graphics information
+    private Stage stage;
+    private BitmapFont font;
+    private TextureAtlas buttonAtlas;
+    private Skin skin;
+    private TextButtonStyle textButtonStyle;
+    private TextButton button;
+    private TextField textfield;
+
     @Override
     public void create()
     {
+        // Create simulation
         _blueprint = WorldBlueprint.makeDefault();
         _blueprint.setSize(SIZE);
         _world = new World(_blueprint);
 
+        // Low level grahpics
         _batch = new SpriteBatch();
         _img = new Texture("sprites.png");
+
+        // High level graphics
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        // A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but
+        // strongly
+        // recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+        skin = new Skin();
+
+        // Generate a 1x1 white texture and store it in the skin named "white".
+        Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("white", new Texture(pixmap));
+
+        // Store the default libgdx font under the name "default".
+        skin.add("default", new BitmapFont());
+
+        // Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't
+        // overwrite the font.
+        TextButtonStyle textButtonStyle = new TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+
+        // Create a table that fills the screen. Everything else will go inside this table.
+        Table table = new Table();
+        table.setX(-600);
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        // Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than
+        // "default".
+        final TextButton button = new TextButton("Click me!", skin);
+        button.setX(0);
+        table.add(button);
+
+        // Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when
+        // clicked,
+        // Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will
+        // be reverted.
+        // ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event
+        // won't
+        // revert the checked state.
+        button.addListener(new ChangeListener()
+        {
+            @Override
+            public void changed(ChangeEvent event, Actor actor)
+            {
+                System.out.println("Clicked! Is checked: " + button.isChecked());
+                button.setText("Good job!");
+            }
+        });
+
+        // Add an image actor. Have to set the size, else it would be the size of the drawable (which is the 1x1
+        // texture).
+        table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
     }
 
     @Override
@@ -53,6 +138,9 @@ public class Vivarium extends ApplicationAdapter
 
         _batch.end();
 
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+
         tick++;
         if (tick > 0)
         {
@@ -66,7 +154,7 @@ public class Vivarium extends ApplicationAdapter
 
     private void drawSprite(VivariumSprite sprite, int xPos, int yPos, float angle)
     {
-        float x = xPos * BLOCK_SIZE;
+        float x = SIZE / 2 * BLOCK_SIZE + xPos * BLOCK_SIZE;
         float y = getHeight() - yPos * BLOCK_SIZE - BLOCK_SIZE;
         float originX = BLOCK_SIZE / 2;
         float originY = BLOCK_SIZE / 2;
