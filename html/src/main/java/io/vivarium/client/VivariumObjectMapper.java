b14637b350d037d3b380b76912610aff8fa51fd4
John H. Uckele <j@vivarium.io>
2016-07-28 22:17:32 -0400

    Removed clone core/html projects, replaced vivarium-html with html (renamed to vivarium-html)

--- html/src/main/java/io/vivarium/client/VivariumObjectMapper.java
+++
@@ -1,9 +0,0 @@
-package io.vivarium.client;
-
-import com.github.nmorel.gwtjackson.client.ObjectMapper;
-
-import io.vivarium.serialization.VivariumObject;
-
-public interface VivariumObjectMapper extends ObjectMapper<VivariumObject>
-{
-}
