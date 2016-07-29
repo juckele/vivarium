92e374e58faada4d5d2aa5d8eafefbd24d8f719a
John H. Uckele <j@vivarium.io>
2016-07-28 21:26:04 -0400

    html compiling like vivarium-html project

--- html/src/main/java/io/vivarium/client/HtmlLauncher.java
+++ html/src/main/java/io/vivarium/client/HtmlLauncher.java
@@ -3,7 +3,9 @@ package io.vivarium.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import io.vivarium.html.Vivarium;
+
+import io.vivarium.serialization.VivariumObjectCopier;
+import io.vivarium.visualizer.Vivarium;
 
 public class HtmlLauncher extends GwtApplication
 {
@@ -11,12 +13,13 @@ public class HtmlLauncher extends GwtApplication
     @Override
     public GwtApplicationConfiguration getConfig()
     {
-        return new GwtApplicationConfiguration(480, 320);
+        return new GwtApplicationConfiguration((int) (Vivarium.getWidth() * 1.5), Vivarium.getHeight());
     }
 
     @Override
     public ApplicationListener createApplicationListener()
     {
-        return new Vivarium();
+        VivariumObjectCopier copier = new StreamingObjectCopier();
+        return new Vivarium(copier);
     }
 }
---
+++ html/src/main/java/io/vivarium/client/VivariumObjectMapper.java
@@ -0,0 +1,9 @@
+package io.vivarium.client;
+
+import com.github.nmorel.gwtjackson.client.ObjectMapper;
+
+import io.vivarium.serialization.VivariumObject;
+
+public interface VivariumObjectMapper extends ObjectMapper<VivariumObject>
+{
+}
