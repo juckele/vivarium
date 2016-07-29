e4370494dd80b1e9ac3dfc8e44e73759ecb0791c
John H. Uckele <j@vivarium.io>
2016-07-28 20:04:56 -0400

    Added some files copied from a default LibGDX Build
    This correctly builds with "gradle html:superdev".
    
    TODO: Copy vivarium-html files and configs to html until
    html builds Vivarium core

---
+++ html/src/io/vivarium/html/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package io.vivarium.html.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import io.vivarium.html.Vivarium;
+
+public class HtmlLauncher extends GwtApplication
+{
+
+    @Override
+    public GwtApplicationConfiguration getConfig()
+    {
+        return new GwtApplicationConfiguration(480, 320);
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener()
+    {
+        return new Vivarium();
+    }
+}
